e9dc8901ae174e5fa88f954b7617f9aeddaff63d
shalant <doug.rosenberg@gmail.com>
2023-09-18 21:14:08 -0500

    more styling updates to mobile-first layout. this is coming together

---
+++ BloggieToBike/Pages/OldNav18Sep.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BloggieToBike.Pages
+{
+    public class OldNav18SepModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
